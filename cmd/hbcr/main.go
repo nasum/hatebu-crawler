def1c243286358c095e50aba62d0478820a53748
nasum <tomato.wonder.life@gmail.com>
2021-07-31 01:24:50 +0900

    add sub command

--- cmd/hbcr/main.go
+++ cmd/hbcr/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
 	c := colly.NewCollector()
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		fmt.Println("Link: %q -> %s\n", e.Text, link)
-	})
+	if len(os.Args) < 2 {
+		os.Exit(0)
+	}
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("visitting", r.URL.String())
-	})
+	switch os.Args[1] {
+	case "user":
+		cmd := flag.NewFlagSet("user", flag.ExitOnError)
+		target := cmd.String("target", "", "crawl target")
+		err := cmd.Parse(os.Args[2:])
 
-	c.Visit("https://b.hatena.ne.jp/")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(*target)
+	case "top":
+		c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+			link := e.Attr("href")
+			fmt.Printf("Link: %s\n", link)
+		})
+
+		c.OnRequest(func(r *colly.Request) {
+			fmt.Println("visitting", r.URL.String())
+		})
+
+		err := c.Visit("https://b.hatena.ne.jp/")
+
+		if err != nil {
+			fmt.Printf("err: %s", err)
+		}
+	}
 }
